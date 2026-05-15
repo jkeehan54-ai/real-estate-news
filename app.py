772773a6b3d3ef737828f5ca6bc628bfb2dc70e3
jkeehan54-ai <jkeehan54@gmail.com>
2026-05-15 14:00:48 +0900

    fix app syntax error

--- app.py
+++ app.py
@@ -118,10 +118,11 @@ def refresh_news():
 
 if __name__ == "__main__":
 
-    app.run(
-        host="0.0.0.0",
-        port=10000
-    )
+    port = int(
+        os.environ.get(
+            "PORT",
+            10000
+        )
     )
 
     app.run(
