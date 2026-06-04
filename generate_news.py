a517df0125081ce74157dd3a220337fb4c166a38
jkeehan54-ai <jkeehan54@gmail.com>
2026-06-04 16:26:05 +0900

    Update generate_news.py

--- generate_news.py
+++ generate_news.py
@@ -1,4 +1,11 @@
 import feedparser, requests, re, os
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 from urllib.parse import quote_plus
 from difflib import SequenceMatcher
 from datetime import datetime, timezone, timedelta
