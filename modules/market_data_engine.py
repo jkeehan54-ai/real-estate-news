e8fbd55302d3739452c697957b7b8d4d49733469
jkeehan54-ai <jkeehan54@gmail.com>
2026-07-24 18:11:57 +0900

    Update market_data_engine.py

--- modules/market_data_engine.py
+++ modules/market_data_engine.py
@@ -1,1 +1,27 @@
-#
+# modules/market_data_engine.py
+
+from modules.kb_market import get_market_brief
+from modules.reb_market import REBMarket
+
+
+class MarketDataEngine:
+    def __init__(self):
+        self.reb = REBMarket()
+
+    def build(self):
+        indicators = {}
+
+        reb = self.reb.get()
+
+        if reb:
+            indicators["PRICE_INDEX"] = reb.get("전국", 0.0)
+            indicators["SEOUL_PRICE_INDEX"] = reb.get("서울", 0.0)
+            indicators["BUSAN_PRICE_INDEX"] = reb.get("부산", 0.0)
+        else:
+            indicators["PRICE_INDEX"] = 0.0
+            indicators["SEOUL_PRICE_INDEX"] = 0.0
+            indicators["BUSAN_PRICE_INDEX"] = 0.0
+
+        indicators["KB_MARKET"] = get_market_brief()
+
+        return indicators
